137b7abb6c89cb3d1445d8aee5227f3019bf3895
fusion809 <brentonhorne77@gmail.com>
2017-02-06 03:12:04 +1000

    Revert "Rm icons better rep as svg"
    This reverts commit d53f0a90acd254d511cd15d39c6a12387e82165a.

--- apps/scalable/convert.sh
+++ apps/scalable/convert.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
-function convert {
-	for i in "*.svg"
-	do
-		convert -resize 128x128 "$i" ../128/"${i/.*/}".png
-	done
-}
-
-function clean {
-	for i in "*.svg"
-	do
-		rm ../128/${i/.*/}.png
-	done
-}
-
-clean
+for i in "*.svg"
+do
+	convert -resize 128x128 "$i" ../128/"${i/.*/}".png
+done
