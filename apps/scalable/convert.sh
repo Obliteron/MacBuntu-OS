d53f0a90acd254d511cd15d39c6a12387e82165a
fusion809 <brentonhorne77@gmail.com>
2017-02-06 03:11:22 +1000

    Rm icons better rep as svg

--- apps/scalable/convert.sh
+++ apps/scalable/convert.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-for i in "*.svg"
-do
-	convert -resize 128x128 "$i" ../128/"${i/.*/}".png
-done
+function convert {
+	for i in "*.svg"
+	do
+		convert -resize 128x128 "$i" ../128/"${i/.*/}".png
+	done
+}
+
+function clean {
+	for i in "*.svg"
+	do
+		rm ../128/${i/.*/}.png
+	done
+}
+
+clean
